eb75fdef0a2c1339dae00b3827ab9f2f7e66fd24
Datureli <soleris1@gmail.com>
2022-04-29 20:51:49 +0200

    check script and lang error in project section

--- pages/ProjectSection.vue
+++ pages/ProjectSection.vue
@@ -1,54 +1,6 @@
 <template>
   <v-container class="mt-9 mx-auto">
-    <v-carousel show-arrows-on-hover height="250" width="200" class="mt-15 d-grid" hide-delimiters>
-      <v-carousel-item
-      
-        :ripple="{ center: true }"
-        style="filter: contrast(0.5); background-position: bottom center"
-        class="mx-auto"
-        width="500"
-        v-for="(item, i) in items"
-        :key="i"
-      >
-        <v-hover v-slot="{ hover }">
-          <v-card class="mx-auto" color="grey lighten-4" max-width="600">
-            <v-img :src="item.image">
-              <v-expand-transition>
-                <v-slide-x-transition>
-                  <div
-                    v-if="hover"
-                    class="d-grid yellow darken-2 text-center white--text"
-                    style="height: 100%"
-                  >
-                    <v-card-text class="text-h3 pa-10 mx-auto font-weight-bold">
-                      {{ item.title }}</v-card-text
-                    >
-                    <a :href="item.githubLink" target="_blank">
-                      <v-icon x-large>mdi-github</v-icon></a
-                    >
-                    <a :href="item.liveLink" target="_blank">
-                      <v-icon x-large>mdi-camera</v-icon></a
-                    >
-                  </div>
-                </v-slide-x-transition>
-              </v-expand-transition>
-            </v-img>
-          </v-card>
-        </v-hover>
-      </v-carousel-item>
-    </v-carousel>
-
-    <v-card
-      width="200"
-      elevation="0"
-      color="transparent"
-    >
-      <a href="https://github.com/Datureli" target="_blank">
-        <v-icon color="black" style="font-size: 150px" class="mx-auto pulse"
-          >mdi-github</v-icon
-        >
-      </a>
-    </v-card>
+   
   </v-container>
 </template>
 
