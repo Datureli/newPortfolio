d568d92eda4abbfad135bf37dbc0e74129f98376
Datureli <soleris1@gmail.com>
2022-04-30 19:26:07 +0200

    change size of bubble

--- pages/AboutMe.vue
+++ pages/AboutMe.vue
@@ -3,55 +3,57 @@
     <v-row class="justify-center mt-16">
       <v-card color="transparent" elevation="0">
         <v-card-text class="white--text text-h6 mx-auto text-center">
-          <div class="bubble">
-            <p
-              v-text="
-                !firstPartOfText
-                  ? ''
-                  : secondPartOfText
-                  ? 'Technologie które poznałem:'
-                  : 'Znajdziesz mnie na:'
-              "
-            ></p>
+          <Transition>
+            <div class="bubble">
+              <p
+                v-text="
+                  !firstPartOfText
+                    ? ''
+                    : secondPartOfText
+                    ? 'Technologie które poznałem:'
+                    : 'Znajdziesz mnie na:'
+                "
+              ></p>
 
-            <div v-if="!firstPartOfText">
-              <p>
-                Uczę się ekosystemu jakim jest front-end od ponad roku.Obecnie
-                doskonale swoje umiejętności w kierunku Vuejs i z tym
-                frameworkiem chcę związać swoją przyszłość
-              </p>
-            </div>
-            <v-card
-              v-else-if="secondPartOfText"
-              height="10"
-              color="transparent"
-              class="flex-wrap d-flex mx-auto"
-              style="justify-content: space-between"
-            >
+              <div v-if="!firstPartOfText">
+                <p>
+                  Uczę się ekosystemu jakim jest front-end od ponad roku.Obecnie
+                  doskonale swoje umiejętności w kierunku Vuejs i z tym
+                  frameworkiem chcę związać swoją przyszłość
+                </p>
+              </div>
               <v-card
-                height="40"
-                v-for="technology in technologies"
-                :key="technology"
+                v-else-if="secondPartOfText"
+                height="10"
                 color="transparent"
+                class="flex-wrap d-flex mx-auto"
+                style="justify-content: space-between"
               >
-                <v-card-text class="white--text">{{
-                  technology.name
-                }}</v-card-text>
+                <v-card
+                  height="40"
+                  v-for="technology in technologies"
+                  :key="technology"
+                  color="transparent"
+                >
+                  <v-card-text class="white--text">{{
+                    technology.name
+                  }}</v-card-text>
+                </v-card>
               </v-card>
-            </v-card>
 
-            <div
-              v-else-if="thirdPartOfText"
-              v-for="socialMediaLink in socialMediaLinks"
-              :key="socialMediaLink"
-            >
-              <p>
-                <a :href="socialMediaLink.link" target="_blank">{{
-                  socialMediaLink.title
-                }}</a>
-              </p>
+              <div
+                v-else-if="thirdPartOfText"
+                v-for="socialMediaLink in socialMediaLinks"
+                :key="socialMediaLink"
+              >
+                <p>
+                  <a :href="socialMediaLink.link" target="_blank">{{
+                    socialMediaLink.title
+                  }}</a>
+                </p>
+              </div>
             </div>
-          </div>
+          </Transition>
         </v-card-text>
       </v-card>
       <v-card
@@ -60,13 +62,11 @@
         class="reBack text-center mt-12"
         color="transparent"
       >
-        <transition name="home" mode="out-in">
-          <v-row>
-            <v-btn small @click="backToFirstPartOfText">1</v-btn>
-            <v-btn small @click="backToSecondPartOfText">2</v-btn>
-            <v-btn small @click="backToThirdPartOfText">3</v-btn>
-          </v-row>
-        </transition>
+        <v-row>
+          <v-btn small @click="backToFirstPartOfText">1</v-btn>
+          <v-btn small @click="backToSecondPartOfText">2</v-btn>
+          <v-btn small @click="backToThirdPartOfText">3</v-btn>
+        </v-row>
       </v-card>
     </v-row>
   </v-container>
@@ -101,6 +101,10 @@ export default {
         link: "",
       },
       {
+        name: "Vue-router",
+        link: "",
+      },
+      {
         name: "Vuex",
         link: "",
       },
@@ -118,6 +122,14 @@ export default {
       },
       {
         name: "Bootstrap",
+        link: "",
+      },
+      {
+        name: "Git and Github",
+        link: "",
+      },
+      {
+        name: "basic react",
         link: "",
       },
     ],
@@ -176,10 +188,19 @@ export default {
   border: 2px solid white;
 }
 .bubble {
-  width: 450px;
+  width: 430px;
   height: 230px;
   padding: 20px 20px 50px 20px;
   background: url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/476907/speechbubble.svg")
     no-repeat top center;
+}
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
